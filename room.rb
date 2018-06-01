2e77450e022c7e0a9f2e1259ce3616a8c5b9b4d1
jesus2102 <jesusperezmarin@gmail.com>
2018-06-01 11:09:12 +0100

    check in/out complete

--- room.rb
+++ room.rb
@@ -3,8 +3,21 @@ class Room
   def initialize(number)
 
     @room_number = number
+    @playlist = {}
     @number_guests = []
 
+  end
+
+  def guest_count
+    return @number_guests.length()
+  end
+
+  def check_in(guest)
+    @number_guests.push(guest)
+  end
+
+  def check_out(guest)
+    @number_guests.delete(guest)
   end
 
 end
---
+++ specs/room_spec.rb
@@ -0,0 +1,36 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../song.rb")
+require_relative("../room.rb")
+require_relative("../guest.rb")
+
+class RoomTest < MiniTest::Test
+
+  def setup
+    @room = Room.new(01)
+
+    @song1 = Song.new("Vive la vida loca", "Ricky Martin")
+    @song2 = Song.new("Bycicle", "Queens")
+
+    @guest1 = Guest.new("Tony")
+    @guest2 = Guest.new("Zsolt")
+  end
+
+  def test_guest_count
+    assert_equal(0, @room.guest_count())
+  end
+
+  def test_check_in
+    @room.check_in(@guest1)
+    assert_equal(1, @room.guest_count())
+  end
+
+  def test_check_out
+    @room.check_in(@guest1)
+    @room.check_in(@guest2)
+    @room.check_out(@guest1)
+    assert_equal(1, @room.guest_count())
+  end
+
+
+end
